f252683d22eb377cd09bc383e6eab83db95c722a
Tiruye-z <tiruezerjsoul@gmail.com>
2024-03-10 18:48:14 -0400

    Task 2 completed

--- src/main/java/edu/canisius/cyb/cyb600/lab2/Task2.java
+++ src/main/java/edu/canisius/cyb/cyb600/lab2/Task2.java
@@ -1,16 +1,40 @@
 package edu.canisius.cyb.cyb600.lab2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task2 {
 
-    public String method1(String parameter1){
-        return parameter1;
+    public String method1(String stringToReverse) {
+        if (stringToReverse == null) {
+            return "";
+        }
+        StringBuilder reversedString = new StringBuilder(stringToReverse);
+        reversedString.reverse();
+        return reversedString.toString().toUpperCase();
     }
 
-    public String method2(String parameter2){
-        return parameter2;
+    public String method2(String reptedstring) {
+        if (reptedstring == null || reptedstring.equals("")) {
+            return "";
+        }
+        int length = reptedstring.length();
+        char firstLetter = reptedstring.charAt(0);
+        return String.valueOf(firstLetter).repeat(length) + reptedstring;
     }
 
-    public String method3(String parameter3){
-        return parameter3;
+    public String method3(String words) {
+        if (words == null || words.equals("")) {
+            return "";
+        }
+        String[] word = words.split(" "); // Split by spaces
+        StringBuilder reversedString = new StringBuilder();
+        for (int i = word.length - 1; i >= 0; i--) {
+            reversedString.append(word[i]);
+            if (i > 0) {
+                reversedString.append(" "); // Add a space between words
+            }
+        }
+        return reversedString.toString(); // Return the reversed string
     }
 }
